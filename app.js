7bbfd7305df88e4b035e73b4a290d9eb87158d8a
Jacob R <jakeriches1@gmail.com>
2021-11-17 15:02:00 +0000

    updated cors policy

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Configure CORS
 const corsOptions = {
-  origin: ['https://jakerich1.github.io'],
+  origin: ['https://jakerich1.github.io', 'http://localhost:3000'],
   optionsSuccessStatus: 200, // For legacy browser support
 };
 
